a2e6e921edb04d0787d9d20def5467527b433acb
Biblioteca Santa Luzia <biblioteca.sl@senacrjedu.local>
2023-03-14 17:37:19 -0300

    Exercício realizado para o módulo 7

---
+++ src/Cadastro/Impressao.java
@@ -0,0 +1,14 @@
+package Cadastro;
+
+public class Impressao {
+	public static void main(String[] args) {
+		
+		
+	
+		System.out.println(Aluno.getMatricula());
+		System.out.println(Aluno.getNome());
+		System.out.println(Aluno.getIdade());
+
+	
+	}
+}
